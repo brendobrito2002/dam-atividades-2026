7e2c4f03ad766665ef965a80585918ecbafd1271
brendobrito2002 <brendoplayer@gmail.com>
2026-07-11 11:26:23 -0300

    removendo versao da atividade 2 feita em cima da branch errada

--- atividade_02_form_savepass/android/app/src/main/kotlin/com/example/save_pass/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.save_pass
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- atividade_02_form_savepass/lib/src/model/repository/interface/i_password_repository.dart
+++
@@ -1,8 +0,0 @@
-import 'package:save_pass/src/model/password_model.dart';
-
-abstract class IPasswordRepository {
-  Future<PasswordModel> getPassword(String serviceName);
-  Future<void> savePassword(PasswordModel password);
-  Future<void> deletePassword(String serviceName);
-  Future<List<PasswordModel>> getAllPasswords();
-}
