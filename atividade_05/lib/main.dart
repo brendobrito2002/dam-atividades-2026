f335e01f99643f7fe0cfb9b554bfdabe83e990b1
brendobrito2002 <brendoplayer@gmail.com>
2026-07-23 18:11:31 -0300

    chore(atividade 5): configuracao do sqflite e suporte a windows desktop

--- atividade_05/lib/main.dart
+++ atividade_05/lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'src/app.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 
 void main() {
+  sqfliteFfiInit();
+  databaseFactory = databaseFactoryFfi;
+  
   runApp(const App());
 }
